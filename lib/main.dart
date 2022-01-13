87461c048e05f29423c0f190f6a1e66945dc2bb5
KUZAlex80 <avzvyagin1980@gmail.com>
2022-01-13 13:40:01 +0700

    Исправлена ошибка в файле pubspec.yaml, заменены графические файлы и доработан код программы

--- lib/main.dart
+++ lib/main.dart
@@ -12,59 +12,59 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     const borderStyle = OutlineInputBorder(
       borderRadius: BorderRadius.all(Radius.circular(36)),
-      borderSide: BorderSide(color: const Color(0xFFeceff1), width: 2));
+      borderSide: BorderSide(color: Color (0xFFeceff1), width: 2));
     const LinktestSt = TextStyle(fontSize: 18, fontWeight: FontWeight.bold,
-        color: Color(0xFF0079D0));
+        color: Color.fromRGBO(0, 0, 0, 0.6));
     return MaterialApp(
       home: Scaffold(
           body: Container (
+            decoration: const BoxDecoration(image: DecorationImage(
+                image: AssetImage('assets/Fon3.jpg'), fit: BoxFit.cover)),
             width: double.infinity,
-            child: Column(children: [
-              SizedBox(height: 70,),
-              SizedBox(width: 110, height: 84, child: Placeholder(),),
-              SizedBox(height: 20,),
-              Text('Введите логин и пароль, выданный администратором системы',
-                style: TextStyle(fontSize: 16, color: Color.fromRGBO(0, 0, 0, 0.6)),),
-              SizedBox(height: 20,),
-              const SizedBox(width: 224,
-                child: TextField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Color(0xFFeceff1),
-                    enabledBorder: borderStyle,
-                    focusedBorder: borderStyle,
-                    labelText: 'Логин',
+              height: double.infinity,
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+              child: SingleChildScrollView(child: Column(children: [
+                const SizedBox(height: 100,),
+                const SizedBox(width: 150, height: 150, child: Image(
+                    image: AssetImage('assets/logo2.png')),),
+                const SizedBox(height: 20,),
+                const Text('Введите логин и пароль',
+                  style: TextStyle(fontSize: 16, color: Color.fromRGBO(0, 0, 0, 0.6)),),
+                const SizedBox(height: 20,),
+                const TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xFFeceff1),
+                      enabledBorder: borderStyle,
+                      focusedBorder: borderStyle,
+                      labelText: 'Логин',
+                    ),
                   ),
-                ),
-              ),
-              SizedBox(height: 20,),
-              const SizedBox(width: 224,
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Color(0xFFeceff1),
-                    enabledBorder: borderStyle,
-                    focusedBorder: borderStyle,
-                    labelText: 'Пароль',
+                const SizedBox(height: 20,), const TextField(obscureText: true,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xFFeceff1),
+                      enabledBorder: borderStyle,
+                      focusedBorder: borderStyle,
+                      labelText: 'Пароль',
+                    ),
                   ),
+                const SizedBox(height: 28,),
+                SizedBox(width: 154, height: 42,child:
+                ElevatedButton(onPressed: () {},
+                  child: const Text('Вход'),
+                style: ElevatedButton.styleFrom(primary: const Color.fromRGBO(0, 0, 0, 0.6),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(36.0),
+                ),),)
                 ),
-              ),
-              SizedBox(height: 28,),
-              SizedBox(width: 154, height: 42,child:
-              ElevatedButton(onPressed: () {},
-                child: Text('Вход'),
-              style: ElevatedButton.styleFrom(primary: Color(0xFF0079D0),
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(36.0),
-              ),),)
-              ),
-              SizedBox(height: 62,),
-              InkWell(child: Text('Забыли пароль?', style: LinktestSt,),
-                  onTap: () {}),
-              SizedBox(height: 20,),
-              InkWell(child: Text('Обратиться к разработчику', style: LinktestSt,),
-                  onTap: () {}),
-            ],),
+                const SizedBox(height: 62,),
+                InkWell(child: const Text('Забыли пароль?', style: LinktestSt,),
+                    onTap: () {}),
+                const SizedBox(height: 20,),
+                InkWell(child: const Text('Обращение к разработчику', style: LinktestSt,),
+                    onTap: () {}),
+              ],),
+            ),
           )
       ),
     );
